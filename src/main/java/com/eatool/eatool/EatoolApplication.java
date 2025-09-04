a7e2a8a0ef34a2345ae019328d3656ac9863e9f7
Uzair Anjum <uzair.anjum@pwc.com>
2025-09-04 19:04:27 +0200

    Initial commit

---
+++ src/main/java/com/eatool/eatool/EatoolApplication.java
@@ -0,0 +1,13 @@
+package com.eatool.eatool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EatoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eatool/eatool/EatoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eatool.eatool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
